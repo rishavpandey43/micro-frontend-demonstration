36251c3c394c7a161dc4d0d0334afe43ac265276
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2022-04-17 20:39:01 +0530

    :fire: remove files from master branch

--- employer/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import AppRoute from "./AppRoute";
-
-import "./index.scss";
-
-const App = () => <AppRoute />;
-ReactDOM.render(<App />, document.getElementById("app"));
